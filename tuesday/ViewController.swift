f495d7b6e6072e687a2e535ccce129567beea858
Nathan Cazell <nathancazell@gmail.com>
2014-12-18 22:00:53 -0500

    Initial Commit

---
+++ tuesday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tuesday
+//
+//  Created by Nathan Cazell on 12/18/14.
+//  Copyright (c) 2014 Nathan Cazell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
