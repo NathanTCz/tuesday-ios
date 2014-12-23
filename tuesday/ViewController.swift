43c1e9f0a956ffcd22178fe34aad6631041f7036
Nathan Cazell <nathancazell@gmail.com>
2014-12-22 22:07:41 -0500

    first commit

--- tuesday/ViewController.swift
+++ tuesday/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var countdown: UILabel!
+    @IBOutlet var feed: UITableView!
+    
+    // messages array
+    var chats: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.feed.registerClass(UITableViewCell.self, forCellReuseIdentifier: "chat")
+        
+        self.countdown.text = "110:22:33"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // chat input logic
+    @IBAction func newChat(sender: UITextField) {
+        chats.append(sender.text)
+        
+        self.feed.reloadData()
+        sender.text = ""
+    }
 
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.chats.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.feed.dequeueReusableCellWithIdentifier("chat") as UITableViewCell
+        
+        cell.textLabel?.text = self.chats[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 }
 
---
+++ tuesday/tuesday-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  tuesday-Bridging-Header.h
+//  tuesday
+//
+//  Created by Nathan Cazell on 12/20/14.
+//  Copyright (c) 2014 Nathan Cazell. All rights reserved.
+//
+
+#ifndef tuesday_tuesday_Bridging_Header_h
+#define tuesday_tuesday_Bridging_Header_h
+
+#import "SocketRocket/SRWebSocket.h"
+
+#endif
